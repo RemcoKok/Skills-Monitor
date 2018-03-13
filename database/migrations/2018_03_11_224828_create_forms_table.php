0932fa1fbebd1671f76bdb5e1563589d5982f2ec
Remco Kok <kok0047@hz.nl>
2018-03-13 10:39:43 +0100

    With this commit I add the rows, cells and forms tables to the database

---
+++ database/migrations/2018_03_11_224828_create_forms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->increments('fromId');
+            $table->string('formName');
+            $table->integer('deadline');
+            $table->integer('class');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+}
---
+++ database/migrations/2018_03_11_224908_create_rows_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rows', function (Blueprint $table) {
+            // $table->foreign('fromId')->references('formId')->on('forms');
+            $table->increments('rowId');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rows');
+    }
+}
