222c219acd84ba097e6641f7aa678abde7ef4469
RemcoKok <36231042+RemcoKok@users.noreply.github.com>
2018-05-13 15:43:11 +0200

    Revert "I add a page to fill in forms"

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Rating;
-use Auth;
-use DB;
 
 class ScoreController extends Controller
 {
@@ -26,34 +23,7 @@ class ScoreController extends Controller
      */
     public function create()
     {
-        $id = Auth::user()->id;
-
-        $forms = DB::table('ratings')
-        ->where('empty_forms.title', "da")
-        ->join('empty_forms', 'ratings.emptyForm_id', '=', 'empty_forms.id')
-        ->get();
-
-        $rows = DB::table('empty_forms')
-        ->select('rows.*')
-        ->where('empty_forms.title', "da")
-        ->join('rows', 'rows.emptyForm_id', '=', 'empty_forms.id')
-        ->get();
-       
-
-        $cells = DB::table('empty_forms')
-        ->select('cells.*')
-        ->where('empty_forms.title', "da")
-        ->join('rows', 'rows.emptyForm_id', '=', 'empty_forms.id')
-        ->join('cells', 'cells.row_id', '=', 'rows.id')
-        ->get();
-        //->where('id', $ratingID)->get()
-        // $ratingID = Rating::all();
-
-        // $forms = emptyForm::all();
-
-        // $rows = Row::all();
-
-        return view('score.create', compact('forms','rows', 'cells'));
+        //
     }
 
     /**
