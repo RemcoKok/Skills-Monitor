86197107068a37b241d3bccbd2688d7e654965de
Remco Kok <kok0047@hz.nl>
2018-05-24 10:30:38 +0200

    I change the database.php file for heroku

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+     'default' => 'pgsql', 
 
     /*
     |--------------------------------------------------------------------------
@@ -54,18 +54,18 @@ return [
             'engine' => null,
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
+        'pgsql' => [ 
+             'driver' => 'pgsql', 
+            'host' => parse_url(getenv("DATABASE_URL"))["host"], 
+            // 'port' => env('DB_PORT', '5432'), 
+            'database' =>substr(parse_url(getenv("DATABASE_URL"))["path"],1), 
+            'username' => parse_url(getenv("DATABASE_URL"))["user"], 
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"], 
+            'charset' => 'utf8', 
+            'prefix' => '', 
+            'schema' => 'public', 
+            'sslmode' => 'prefer', 
+        ], 
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
