222fb00237afa74818305e11a67a702e1daa70fa
Sophie van Turenhout <sophieturenhout@gmail.com>
2018-06-05 14:06:33 +0200

    i add a solution to a problem

--- resources/views/forms/edit.blade.php
+++ resources/views/forms/edit.blade.php
@@ -7,20 +7,20 @@
 @stop
 
 @section('content')
-<div class="box-body">
-            <div class="box-header with-border">
-                <h3> Naam formulier:
-                    <tr>
-                        <td>{{$empty_form->title}}</td>
-                    </tr>
-                </h3>
-                <h3> Competentie:
-                    <tr>
-                        <td>{{$empty_form->competence_id}}</td>
-                    </tr>
-                </h3>
-                </div> 
-<div class="row">
+    <div class="box-body">
+        <div class="box-header with-border">
+            <h3> Naam formulier:
+                <tr>
+                    <td>{{$empty_form->title}}</td>
+                </tr>
+            </h3>
+            <h3> Competentie:
+                <tr>
+                    <td>{{$empty_form->competence_id}}</td>
+                </tr>
+            </h3>
+        </div>
+        <div class="row">
             <div class="col-md-12">
                 <br/>
                 <h3 align="left">Voer naam en competentie in</h3>
@@ -35,7 +35,7 @@
                     </div>
                 @endif
 
-                <form method="post" action="{{route('form.update',['id'=>$empty_form->id)]}}">
+                <form method="post" action="{{route('form.update',['id'=>$empty_form->id])}}">
                     {{csrf_field()}}
                     @method('PUT')
                     <div class="form-group">
@@ -45,9 +45,10 @@
                         <input type="text" class="form-control" placeholder="Enter competence" name="competence_id"/>
                     </div>
                     <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Verzenden</button>
+                        <button type="submit" class="btn btn-primary">Verzenden</button>
                     </div>
                 </form>
             </div>
         </div>
+    </div>
 @stop
